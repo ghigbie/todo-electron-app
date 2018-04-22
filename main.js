7e421e10e5b9a4ddad2094af483f1701209bff2a
ghigbie <gehigbie@gmail.com>
2018-04-21 19:16:56 -0600

    Edit files

--- main.js
+++ main.js
@@ -2,7 +2,10 @@ const electron = require('electron');
 
 const { app, BrowserWindow } = electron;
 
+let mainWindow;
+
 app.on('ready', () => {
     console.log('Electron is running');
-    new BrowserWindow({});
+    mainWindow = new BrowserWindow({});
+    mainWindow.loadURL(`file://${__dirname}/main.html`);
 });
