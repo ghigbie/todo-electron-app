ed8ae7c6cd43685b7909c0db9a0a02c4d6c00ebb
ghigbie <gehigbie@gmail.com>
2018-04-21 20:13:09 -0600

    Add menu template

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const electron = require('electron');
 
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, Menu } = electron;
 
 let mainWindow;
 
@@ -8,4 +8,11 @@ app.on('ready', () => {
     console.log('Electron is running');
     mainWindow = new BrowserWindow({}); //the empty object is for configuration options
     mainWindow.loadURL(`file://${__dirname}/main.html`);
+    const mainMenu = Menu.buildFromTemplate(menuTemplate);
 });
+
+const menuTemplate = [
+    {
+        lable: 'File'
+    }
+]
